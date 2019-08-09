eb167da3942add4c1ae8dd5ae9546ffd0a3e80e8
15601090990 <15601090990@163.com>
2019-08-09 18:03:45 +0800

    Update url_all.js

--- dianjiProduct/DianJi/DianJi/js/url_all.js
+++ dianjiProduct/DianJi/DianJi/js/url_all.js
@@ -6,15 +6,15 @@
 //var url = 'http://192.168.2.246:8080/server_appapi/';
 //测试服务器地址
 
-// var url = 'http://39.106.180.14:1111/';
+var url = 'http://39.106.180.14:1111/';
 
 //生产服务器地址
-var url = 'http://47.94.166.103:1111/';
+// var url = 'http://47.94.166.103:1111/';
 
 //var fileUrlHeader = "http://47.94.166.103:3000/group1/";
 
 
-var strLocalVersion = '1.11.5.0';
+var strLocalVersion = '1.11';
 var strLocalVersionCode = 1992;
              
 //登录接口
